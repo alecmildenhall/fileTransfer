734049c6e5bcb5fb70abc3c3928001f2fa8c9703
Amber Mildenhall <ambermildenhall@gmail.com>
2023-03-22 22:13:48 -0400

    2.1 progress

--- FileApp.py
+++ FileApp.py
@@ -12,7 +12,7 @@ import sys
 #   - server needs to be started before the clients can start coming online
 
 # server has to maintain a table (nick-names of clients + files they are sharing + their IP addresses + port numbers)
-server_started = False
+#server_started = False
 
 try:
     mode = sys.argv[1]
@@ -31,41 +31,49 @@ if (mode == "-s"):
 
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind(('', port))
-    #serverSocket.listen(3) # Accepts 3 sequential connections
 
     table = []
     # names, online-status, IPaddresses, TCP and UDP port numbers, and filenames
 
-    server_started = True
-
     # receives registration request
-    message, clientAddress = serverSocket.recvfrom(2048)
+    while True:
+        message, clientAddress = serverSocket.recvfrom(2048)
+        message = message.decode()
 
-    try:
-        client_info = message.split()
-        client_name = client_info[0]
-        client_udp = client_info[1]
-        client_tcp = client_info[2]
-        client_status = client_info[3]
-    except:
-        print('invalid registration request')
-        sys.exit() # TODO: this is prob not the right thing to do
-    
-    # check if name already exists
-    for client in table:
-        if client[0] == client_name:
-            print(client_name + ' is already registered')
-            # TODO: send error message and reject registration
+        try:
+            client_info = message.split()
+            client_name = client_info[0]
+            client_udp = client_info[1]
+            client_tcp = client_info[2]
+            client_status = client_info[3]
+        except:
+            print('invalid registration request')
+            continue
+        
+        # check if name already exists
+        name_exists = 0
+        for client in table:
+            if client[0] == client_name:
+                print(client_name + ' is already registered')
+                name_exists = 1
+                break
+        
+        if (name_exists):
+            continue
 
-    # add client info to table
-    table.append([client_name, client_status, clientAddress, client_tcp, client_udp, []])
+        # add client info to table
+        table.append([client_name, client_status, clientAddress, client_tcp, client_udp, []])
+
+        # send client registered message
+        message = "registered"
+        serverSocket.sendto(message.encode(), clientAddress)
+
+        # send client updated table
+        
 
 
 elif (mode == "-c"):
     # client mode
-    if (not server_started):
-        print('server must be started first')
-        sys.exit()
 
     # get command line arguments
     try:
@@ -79,11 +87,11 @@ elif (mode == "-c"):
         sys.exit()
 
     # check arguments
-    try:
-        socket.inet_aton(server_ip)
-    except:
-        print('invalid IP address')
-        sys.exit()
+    for c in server_ip:
+        if (c != '.' and not str(c).isdigit()):
+            print('invalid IP address')
+            sys.exit()
+    
     
     if ((int(server_port) < 1024 or int(server_port) > 65535) or 
         (int(client_udp_port) < 1024 or int(client_udp_port) > 65535) or
@@ -97,10 +105,15 @@ elif (mode == "-c"):
     # send registration request
     status = 'on'
     message = name + ' ' + client_udp_port + ' ' + client_tcp_port + ' ' + status
-    clientSocket.sendto(message,(server_ip, server_port))
+    clientSocket.sendto(message.encode(),(server_ip, server_port))
 
-    # sucessful client registration?
-    # TODO: do i need prompt first? what makes this successful
+    # receive registration confirmation
+    while True:
+        serverMessage, serverAddress = clientSocket.recvfrom(2048)
+        serverMessage = serverMessage.decode()
+        if (serverMessage == "registered"):
+            break
+    
     print('>>> [Welcome, You are registered.]')
 
     local_table = []
